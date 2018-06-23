ace2c7c10cbf4f8ac3e507449f222d65ac323ed8
Dan Allen <dan@opendevise.com>
2018-06-23 17:41:06 -0600

    add algolia docsearch integration

---
+++ src/js/05-search-toggle.js
@@ -0,0 +1,12 @@
+;(function () {
+  'use strict'
+
+  var searchButton = document.querySelector('button.search')
+  if (!searchButton) return
+  searchButton.addEventListener('click', function (e) {
+    var navbarStart = document.querySelector('.navbar-start')
+    navbarStart.classList.toggle('reveal-search-input')
+    navbarStart.querySelector('input.query').focus()
+    e.preventDefault()
+  })
+})()
