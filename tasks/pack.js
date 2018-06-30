4dfc5849d90c55d5ac11ce5261f804cfffc3d7b7
Dan Allen <dan@opendevise.com>
2018-06-29 18:39:21 -0600

    wrap pack task in Promise so executes in sequence [skip release]

--- tasks/pack.js
+++ tasks/pack.js
@@ -4,7 +4,11 @@ const vfs = require('vinyl-fs')
 const zip = require('gulp-vinyl-zip')
 
 module.exports = async (src, dest, bundleName) =>
-  vfs
-    .src('**/*', { base: src, cwd: src })
-    .pipe(zip.zip(`${bundleName}-bundle.zip`))
-    .pipe(vfs.dest(dest))
+  new Promise((resolve, reject) => {
+    vfs
+      .src('**/*', { base: src, cwd: src })
+      .pipe(zip.zip(`${bundleName}-bundle.zip`))
+      .pipe(vfs.dest(dest))
+      .on('error', reject)
+      .on('end', resolve)
+  })
