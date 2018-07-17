3a23c26a07c5d715895991ca8c1aa3d704a229d2
Dan Allen <dan@opendevise.com>
2018-07-17 15:53:21 -0600

    refactor and simplify nav logic; make fit logic work after resize

--- src/js/01-navigation.js
+++ src/js/01-navigation.js
@@ -1,110 +1,80 @@
 ;(function () {
   'use strict'
 
-  var navMenu = document.querySelector('.nav-menu')
-  if (!navMenu) return
+  var navMenu = {}
+  if (!(navMenu.element = document.querySelector('.nav-menu'))) return
 
-  if (navMenu.classList.contains('fit')) {
-    var footer = document.querySelector('footer')
-    // TODO to make more efficient, recompute base info on window resize
-    // TODO need to set up on load
-    var navMenuStartHeight = navMenu.getBoundingClientRect().height
-    var availableHeight = window.innerHeight
+  var currentPageItem = navMenu.element.querySelector('.is-current-page')
+  if (currentPageItem) expandCurrentPath(currentPageItem)
 
-    window.addEventListener('scroll', function () {
-      var footerVisibleHeight = availableHeight - footer.getBoundingClientRect().top
-      if (footerVisibleHeight > 0) {
-        navMenu.style.height = Math.max(0, (navMenuStartHeight - footerVisibleHeight)) + 'px'
-      } else {
-        navMenu.style.height = ''
-      }
-    })
-  }
-
-  var navState = getNavState()
-  // FIXME look for component and version in head > meta instead
-  var menuState = getMenuState(navState, navMenu.dataset.component, navMenu.dataset.version)
-
-  find('.nav-toggle', navMenu).forEach(function (btn) {
-    // FIXME change this to search ancestors
-    var li = btn.parentElement.parentElement
-    btn.addEventListener('click', function () {
-      li.classList.toggle('is-active')
-      menuState.expandedItems = getExpandedItems(navMenu)
-      saveNavState()
+  find('.nav-toggle', navMenu.element).forEach(function (toggleButton) {
+    var navItem = findAncestorWithClass('nav-item', toggleButton, navMenu.element)
+    toggleButton.addEventListener('click', function () {
+      navItem.classList.toggle('is-active')
     })
   })
 
-  find('.nav-item', navMenu).forEach(function (item, idx) {
-    item.setAttribute('data-id', 'menu-' + item.dataset.depth + '-' + idx)
-  })
-
-  var expandedItems = menuState.expandedItems || (menuState.expandedItems = [])
-
-  var currentPageItem = navMenu.querySelector('.is-current-page')
-  if (currentPageItem) {
-    activateCurrentPath(currentPageItem).forEach(function (itemId) {
-      if (expandedItems.indexOf(itemId) < 0) expandedItems.push(itemId)
-    })
+  if (navMenu.element.classList.contains('fit')) {
+    window.addEventListener('load', fitNavMenuInit)
+  } else if (currentPageItem) {
+    scrollItemIntoView(currentPageItem.querySelector('.nav-link'), navMenu.element)
   }
 
-  saveNavState()
-
-  scrollItemIntoView(0, navMenu, currentPageItem && currentPageItem.querySelector('.nav-link'))
-
-  function activateCurrentPath (navItem) {
-    var ids = [navItem.dataset.id]
-    var ancestorClasses
-    var ancestor = navItem.parentNode
-    // TODO could compare to navMenu here
-    while (!(ancestorClasses = ancestor.classList).contains('nav-menu')) {
-      if (ancestor.tagName === 'LI' && ancestorClasses.contains('nav-item')) {
-        ancestorClasses.add('is-active', 'is-current-path')
-        ids.push(ancestor.dataset.id)
-      }
-      ancestor = ancestor.parentNode
-    }
+  function expandCurrentPath (navItem) {
     navItem.classList.add('is-active')
-    return ids
-  }
-
-  function getExpandedItems (scope) {
-    return find('.is-active', scope).map(function (item) {
-      return item.dataset.id
-    })
-  }
-
-  function getNavState () {
-    var data = window.sessionStorage.getItem('nav-state')
-    return data && (data = JSON.parse(data)).__version__ === '1' ? data : { __version__: '1' }
-  }
-
-  function getMenuState (navState, component, version) {
-    var key = version + '@' + component
-    return navState[key] || (navState[key] = {})
-  }
-
-  function saveNavState () {
-    window.sessionStorage.setItem('nav-state', JSON.stringify(navState))
-  }
-
-  function scrollItemIntoView (scrollPosition, parent, el) {
-    if (!el) return (parent.scrollTop = scrollPosition)
-
-    var margin = 8
-    var overTheTop = el.offsetTop - scrollPosition < 0
-    var belowTheBottom = el.offsetTop - scrollPosition + el.offsetHeight > parent.offsetHeight
-
-    if (overTheTop) {
-      parent.scrollTop = el.offsetTop - margin
-    } else if (belowTheBottom) {
-      parent.scrollTop = el.offsetTop - (parent.offsetHeight - el.offsetHeight) + margin
-    } else {
-      parent.scrollTop = scrollPosition
+    var ancestorClasses
+    var ancestor = navItem
+    while ((ancestor = ancestor.parentNode) !== navMenu.element) {
+      if ((ancestorClasses = ancestor.classList).contains('nav-item')) {
+        ancestorClasses.add('is-active', 'is-current-path')
+      }
     }
+  }
+
+  function scrollItemIntoView (el, parentEl) {
+    var belowBottom = (el.offsetTop + el.offsetHeight + 8) - parentEl.offsetHeight
+    if (belowBottom > 0) parentEl.scrollTop = belowBottom
+  }
+
+  function fitNavMenuInit (e) {
+    window.removeEventListener('scroll', fitNavMenuOnScroll)
+    navMenu.element.style.height = ''
+    if ((navMenu.preferredHeight = navMenu.element.getBoundingClientRect().height) > 0) {
+      if (!navMenu.encroachingElement) navMenu.encroachingElement = document.querySelector('footer.footer')
+      fitNavMenu(navMenu.preferredHeight, (navMenu.viewHeight = window.innerHeight), navMenu.encroachingElement)
+      if (e.type === 'load' && currentPageItem) {
+        scrollItemIntoView(currentPageItem.querySelector('.nav-link'), navMenu.element)
+      }
+      window.addEventListener('scroll', fitNavMenuOnScroll)
+    } else {
+      window.removeEventListener('scroll', fitNavMenuOnScroll)
+    }
+    if (e.type === 'load') {
+      window.removeEventListener('load', fitNavMenuInit)
+      window.addEventListener('resize', fitNavMenuInit)
+    }
+  }
+
+  function fitNavMenuOnScroll () {
+    fitNavMenu(navMenu.preferredHeight, navMenu.viewHeight, navMenu.encroachingElement)
+  }
+
+  function fitNavMenu (preferredHeight, availableHeight, encroachingElement) {
+    var reclaimedHeight = availableHeight - encroachingElement.getBoundingClientRect().top
+    navMenu.element.style.height = reclaimedHeight > 0 ? Math.max(0, (preferredHeight - reclaimedHeight)) + 'px' : ''
   }
 
   function find (selector, from) {
     return [].slice.call((from || document).querySelectorAll(selector))
+  }
+
+  function findAncestorWithClass (className, from, scope) {
+    if ((from = from.parentNode) !== scope) {
+      if (from.classList.contains(className)) {
+        return from
+      } else {
+        return findAncestorWithClass(className, from, scope)
+      }
+    }
   }
 })()
