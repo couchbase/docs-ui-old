e7bdfb35d3f97a1ef81bd39f4d2aa2b52032bba7
Dan Allen <dan@opendevise.com>
2018-09-02 18:01:07 -0600

    further defer loading of docsearch

--- src/js/vendor/docsearch.js
+++ src/js/vendor/docsearch.js
@@ -1,15 +1,17 @@
 ;(function () {
   var docsearch = require('docsearch.js/dist/cdn/docsearch.js')
-  var config = document.getElementById('search-script').dataset
-  var style = document.createElement('style')
-  style.innerText = '@import "' + config.stylesheet + '"'
-  document.body.appendChild(style)
-  docsearch({
-    appId: config.appId,
-    apiKey: config.apiKey,
-    indexName: config.indexName,
-    inputSelector: '#search-query',
-    algoliaOptions: { hitsPerPage: 25 },
-    debug: false,
-  })
+  window.setTimeout(function () {
+    var config = document.getElementById('search-script').dataset
+    var style = document.createElement('style')
+    style.innerText = '@import "' + config.stylesheet + '"'
+    document.head.appendChild(style)
+    docsearch({
+      appId: config.appId,
+      apiKey: config.apiKey,
+      indexName: config.indexName,
+      inputSelector: '#search-query',
+      algoliaOptions: { hitsPerPage: 25 },
+      debug: false,
+    })
+  }, 1000)
 })()
