25e960dc4f7e36a6753985e84e937eac45cd4d08
Dan Allen <dan@opendevise.com>
2018-09-03 00:45:34 -0600

    load docsearch on load

--- src/js/vendor/docsearch.js
+++ src/js/vendor/docsearch.js
@@ -2,23 +2,25 @@
   'use strict'
 
   var docsearch = require('docsearch.js/dist/cdn/docsearch.js')
-  var config = document.getElementById('search-script').dataset
-  var link = document.createElement('link')
-  link.rel = 'stylesheet'
-  link.href = config.stylesheet
-  document.head.appendChild(link)
-  var ds = docsearch({
-    appId: config.appId,
-    apiKey: config.apiKey,
-    indexName: config.indexName,
-    inputSelector: '#search-query',
-    algoliaOptions: { hitsPerPage: 25 },
-    debug: false,
-  })
-  document.querySelector('button.search').addEventListener('click', function (e) {
-    if (document.querySelector('.navbar-start').classList.toggle('reveal-search-input')) {
-      ds.autocomplete.autocomplete.setVal('')
-      ds.input.focus()
-    }
+  window.addEventListener('load', function () {
+    var config = document.getElementById('search-script').dataset
+    var link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = config.stylesheet
+    document.head.appendChild(link)
+    var ds = docsearch({
+      appId: config.appId,
+      apiKey: config.apiKey,
+      indexName: config.indexName,
+      inputSelector: '#search-query',
+      algoliaOptions: { hitsPerPage: 25 },
+      debug: false,
+    })
+    document.querySelector('button.search').addEventListener('click', function (e) {
+      if (document.querySelector('.navbar-start').classList.toggle('reveal-search-input')) {
+        ds.autocomplete.autocomplete.setVal('')
+        ds.input.focus()
+      }
+    })
   })
 })()
