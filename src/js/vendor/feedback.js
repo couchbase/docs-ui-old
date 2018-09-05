35f01095ae82593c23103c3d51be97af8b890472
Dan Allen <dan@opendevise.com>
2018-09-04 18:31:14 -0600

    don't load feedback button on mobile device (since it isn't show anyway)

--- src/js/vendor/feedback.js
+++ src/js/vendor/feedback.js
@@ -1,6 +1,8 @@
 ;(function () {
   'use strict'
 
+  if (Math.max(window.screen.availHeight, window.screen.availWidth) < 769) return
+
   window.addEventListener('load', function () {
     var config = document.getElementById('feedback-script').dataset
     var script = document.createElement('script')
