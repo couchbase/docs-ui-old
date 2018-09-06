8fd783fc9c6521aa894c86fd9ec46bafe3a75c7e
Dan Allen <dan@opendevise.com>
2018-09-05 20:14:52 -0600

    reduce relativize helper to a single argument; retrieve page url from context

--- src/helpers/relativize.js
+++ src/helpers/relativize.js
@@ -2,16 +2,17 @@
 
 const { posix: path } = require('path')
 
-// TODO memoize
-module.exports = (from, to) => {
-  if (!from || !to || to.charAt() === '#') return (to || '#')
+module.exports = (to, { data: { root } }) => {
+  if (!to) return '#'
+  const from = root.page.url
+  if (!from || to.charAt() === '#') return to
   let hash = ''
   const hashIdx = to.indexOf('#')
   if (~hashIdx) {
     hash = to.substr(hashIdx)
     to = to.substr(0, hashIdx)
   }
-  if (from === to) {
+  if (to === from) {
     return hash || (isDir(to) ? './' : path.basename(to))
   } else {
     return path.relative(path.dirname(from + '.'), to) + (isDir(to) ? '/' + hash : hash)
