fe9a7aab383bf2ebec48cba0af98aa0d548142e3
Dan Allen <dan@opendevise.com>
2018-09-27 04:23:27 -0600

    load self-hosted OneTrust cookie consent script when configured

---
+++ src/js/vendor/optanon.js
@@ -0,0 +1,21 @@
+;(function () {
+  'use strict'
+  require('zepto/src/zepto')
+  require('zepto/src/selector')
+  require('zepto/src/event')
+  require('zepto/src/ajax')
+  var $ = window.$
+  $.extend($.fn, {
+    animate: $.fn.css,
+    fadeIn: $.fn.show,
+    fadeOut: $.fn.hide,
+    outerHeight: function () {
+      var el = this
+      return ['padding-top', 'padding-bottom', 'border-top-width', 'border-bottom-width'].reduce(function (acc, prop) {
+        return (acc += parseInt(el.css(prop)))
+      }, el.height())
+    },
+  })
+  require('./optanon-impl.min.js')
+  window.OptanonWrapper = function () {}
+})()
