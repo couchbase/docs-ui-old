4aa04dacc99d9117a5b1ef7c5f3bc7048ab73471
Dan Allen <dan@opendevise.com>
2018-10-03 01:46:45 -0600

    fix branch name in edit URL for page in home component

--- src/helpers/home-edit-url.js
+++ src/helpers/home-edit-url.js
@@ -2,4 +2,5 @@
 
 const SEARCH_RX = /^.*(?=\/home\/)/
 
-module.exports = (editUrl, baseUrl, branch) => editUrl.replace(SEARCH_RX, `${baseUrl}/edit/${branch}`)
+module.exports = (editUrl, baseUrl, branch) =>
+  editUrl.replace(SEARCH_RX, `${baseUrl}/edit/${branch === 'HEAD' ? 'master' : branch}`)
