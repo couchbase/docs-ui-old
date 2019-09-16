bb99d01bd11507a73908e2d06dc8066281388322
Amarantha Kulkarni <amarantha-k@users.noreply.github.com>
2019-09-16 10:28:01 -0700

    Update feedback.js

--- src/js/vendor/feedback.js
+++ src/js/vendor/feedback.js
@@ -10,9 +10,5 @@
    // script.src = 'https://issues.couchbase.com/s/66cd330397e6b28e6a44bd3d603733a8-T/j9sjl8/802003/be0e2f3d0946caa44cd62a35c9de2f18/3.0.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=' + config.collectorId // prettier-ignore
     script.src = 'https://issues.couchbase.com/s/7a5384f36f7448b0198d6cb0bf62765d-T/tv1ord/803004/be0e2f3d0946caa44cd62a35c9de2f18/3.0.10/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=2025633e'
     document.body.appendChild(script)
-});
-
-
-
   })
 })()
