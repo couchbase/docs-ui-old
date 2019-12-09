7557b33a5539166dcf70978df5770b2e1ee39f8b
rakeshAlgo <rakesh.kumar@algoworks.com>
2019-12-09 16:32:41 +0530

    add class for yes&no button and fix style issues

--- src/js/06-helpfull.js
+++ src/js/06-helpfull.js
@@ -2,19 +2,23 @@
   'use strict'
 
   var dialogBox = document.getElementById('dialogBox')
-  var helpBtn = document.getElementById('yesBtn')
-  helpBtn.addEventListener('click', function (e) {
+  var helpYesBtn = document.getElementById('yesBtn')
+  helpYesBtn.addEventListener('click', function (e) {
     dialogBox.style.display = 'block'
+    this.classList.add('active')
+    helpNoBtn.classList.remove('active')
   })
 
   var helpNoBtn = document.getElementById('noBtn')
   helpNoBtn.addEventListener('click', function (e) {
     dialogBox.style.display = 'block'
+    this.classList.add('active')
+    helpYesBtn.classList.remove('active')
   })
-  
+
   var skipBtn = document.getElementById('skipBtn')
   skipBtn.addEventListener('click', function (e) {
     dialogBox.style.display = 'none'
   })
-  
+
 })()
