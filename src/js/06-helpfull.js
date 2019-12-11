866cabd00a1c57ae460d5ba1564ad0c0ec1f8dd3
rakeshAlgo <rakesh.kumar@algoworks.com>
2019-12-11 16:01:16 +0530

    feedback like dislike UI and click event  done

--- src/js/06-helpfull.js
+++ src/js/06-helpfull.js
@@ -1,24 +1,50 @@
 ;(function () {
   'use strict'
-
+  
   var dialogBox = document.getElementById('dialogBox')
   var helpYesBtn = document.getElementById('yesBtn')
+  var helpNoBtn = document.getElementById('noBtn')
+  var skipBtnMsg = document.getElementById('skipBtnMsg')
+  var feedBackFormBox = document.getElementById('additionalFeedbackBox')
+  var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
+  var skipLeaveBtn = document.getElementById('skipLeaveBtn')
+  var feedBackMsg = document.querySelector('.feed-back-msg')
+  var submitBtn = document.querySelector('.submit-btn')
+  var leaveYesBtn = document.querySelector('.yes-btn')
+  // for config 
+  var yesBtnData = helpYesBtn.dataset
+  var noBtnData = helpNoBtn.dataset
+
   helpYesBtn.addEventListener('click', function (e) {
     dialogBox.style.display = 'block'
     this.classList.add('active')
     helpNoBtn.classList.remove('active')
+    console.log(yesBtnData, 16)
   })
 
-  var helpNoBtn = document.getElementById('noBtn')
+ 
   helpNoBtn.addEventListener('click', function (e) {
     dialogBox.style.display = 'block'
     this.classList.add('active')
     helpYesBtn.classList.remove('active')
+    console.log(noBtnData, 27)
   })
 
-  var skipBtn = document.getElementById('skipBtn')
-  skipBtn.addEventListener('click', function (e) {
-    dialogBox.style.display = 'none'
+  skipBtnMsg.addEventListener('click', function (e) {
+    feedBackFormBox.style.display = 'none'
+    leaveAddtinalBox.style.display = 'block'
+  })
+  skipLeaveBtn.addEventListener('click', function (e) {
+
+    leaveAddtinalBox.style.display = 'none'
+    feedBackFormBox.style.display = 'block'
   })
 
+  feedBackMsg.addEventListener('keyup', function (e) {
+    submitBtn.classList.remove('disabled')
+  })
+  leaveYesBtn.addEventListener('click', function (e) {
+    leaveAddtinalBox.style.display = 'none'
+  })
+  
 })()
