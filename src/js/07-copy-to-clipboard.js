2d96fe11cc7a111ab3d491289a0e27c4dc0dc1e8
anmoltripathi <anmol.tripathi@algoworks.com>
2020-01-14 15:24:18 +0530

    update copy-to-clipboard

--- src/js/07-copy-to-clipboard.js
+++ src/js/07-copy-to-clipboard.js
@@ -6,23 +6,25 @@
     //button.type = 'button'
     button.dataset.title = 'Copy'
 
-    button.addEventListener('click', function () {
-      navigator.clipboard.writeText(codeBlock.innerText).then(
-        function () {
-          /* Chrome doesn't seem to blur automatically,
-              leaving the button in a focused state. */
-          button.blur()
+    button.addEventListener('click', function (e) {
+      if (e.target && e.target.matches('a.copy-code-button')) {
+        navigator.clipboard.writeText(codeBlock.innerText).then(
+          function () {
+            /* Chrome doesn't seem to blur automatically,
+                leaving the button in a focused state. */
+            button.blur()
 
-          button.dataset.title = 'Copied!'
+            button.dataset.title = 'Copied!'
 
-          setTimeout(function () {
-            button.dataset.title = 'Copy'
-          }, 2000)
-        },
-        function () {
-          button.dataset.title = 'Error'
-        }
-      )
+            setTimeout(function () {
+              button.dataset.title = 'Copy'
+            }, 2000)
+          },
+          function () {
+            button.dataset.title = 'Error'
+          }
+        )
+      }
     })
     var pre = codeBlock.parentNode
     pre.appendChild(button)
