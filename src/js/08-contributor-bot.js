00c79e9ec293210332f26203652df8072c76b4dd
rakeshAlgo <rakesh.kumar@algoworks.com>
2020-01-24 18:05:33 +0530

    changes in date Formate in contributor

--- src/js/08-contributor-bot.js
+++ src/js/08-contributor-bot.js
@@ -1,20 +1,25 @@
 ;(function () {
   'use strict'
   console.log('contributor page')
+  var showData = false
   var contributors
   var contributorList = document.getElementById('contributorList')
   var commitdateTag = document.getElementById('commitdate')
   var otherContributor = document.getElementById('otherContributor')
-  // var apiURL = 'https://api.github.com'
+  var contributorListBox = document.querySelector('.contributor-list-box')
+  var apiURL = 'https://api.github.com'
+  var githubRepo = '/couchbase/docs-ui'
+  // var url = 'https://api.github.com/repos/couchbase/docs-ui/stats/contributors'
   // var contributorEveryDayURL = 'repos/couchbase/docs-ui/stats/contributors'
   // Call the fetch function passing the url of the API as a parameter
   // eslint-disable-next-line no-undef
-  fetch('https://api.github.com/repos/couchbase/docs-ui/stats/contributors')
+  fetch(apiURL + '/repos' + githubRepo + '/stats/contributors')
     .then(function (resp) {
       var data = resp.json()
       return data
     })
     .then(function (data) {
+      showData = true
       contributors = data
       var otherContributorData = contributors.length - 5
       var maxCommitLength = contributors.length - 1
@@ -24,18 +29,39 @@
       var totaltWeekLength = totalWeek.length - 1 // array
       var lastCommitWeek = totalWeek[totaltWeekLength]
       var lastCommitTimeStamp = lastCommitWeek.w // object
-      var lastCommitDate = new Date(lastCommitTimeStamp * 1000).toDateString() // convert timestamp to date
-      console.log(lastCommitDate, maxCommitAuthor)
-      commitdateTag.append(lastCommitDate)
+      var lastCommitDate = new Date(lastCommitTimeStamp * 1000)// convert timestamp to date
+      var getDate = lastCommitDate.getDate()
+      var getYear = lastCommitDate.getFullYear()
+      // eslint-disable-next-line no-array-constructor
+      var month = new Array()
+      month[0] = 'January'
+      month[1] = 'February'
+      month[2] = 'March'
+      month[3] = 'April'
+      month[4] = 'May'
+      month[5] = 'June'
+      month[6] = 'July'
+      month[7] = 'August'
+      month[8] = 'September'
+      month[9] = 'October'
+      month[10] = 'November'
+      month[11] = 'December'
+      var getMonth = month[lastCommitDate.getMonth()]
+      var latestCommitDate = getMonth + ' ' + getDate + ', ' + getYear
+      commitdateTag.append(latestCommitDate)
       otherContributor.append(otherContributorData)
+
+      // show html data
+      if (showData) {
+        contributorListBox.classList.add('show')
+      }
       // map function
 
       contributors.slice(0).reverse().map(function (contributor, index, arrayobj) {
-        console.log(contributor, index, arrayobj, 19)
         //show only top 5 contributor
         if (index <= 4) {
           var contributorAvtar = contributor.author.avatar_url
-          var contributorTotalCommit = contributor.total
+          // var contributorTotalCommit = contributor.total
           var authorURL = contributor.author.html_url
           // append child in ul
           var li = document.createElement('li')
@@ -48,8 +74,6 @@
           li.appendChild(anchor)
           anchor.appendChild(image)
           contributorList.appendChild(li)
-
-          console.log(contributorAvtar, contributorTotalCommit, 21)
         }
       })
     }).catch(function (err) {
