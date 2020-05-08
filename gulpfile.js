085d1c6051e447d642b2e8e848bd9dca7defbbd2
Dan Allen <dan@opendevise.com>
2020-05-08 03:40:13 -0600

    announce path of bundle in the form of the --ui-bundle-url option; consolidate zip routine to a single call

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const [owner, repo] = new URL(pkg.repository.url).pathname.slice(1).split('/')
 const { parallel, series, watch } = require('gulp')
 const createTask = require('./gulp.d/lib/create-task')
 const exportTasks = require('./gulp.d/lib/export-tasks')
+const log = require('fancy-log')
 
 const bundleName = 'ui'
 const buildDir = ['deploy-preview', 'branch-deploy'].includes(process.env.CONTEXT) ? 'public/dist' : 'build'
@@ -67,7 +68,12 @@ const bundleBuildTask = createTask({
 const bundlePackTask = createTask({
   name: 'bundle:pack',
   desc: 'Create a bundle of the staged UI assets for publishing',
-  call: task.pack(destDir, buildDir, bundleName),
+  call: task.pack(
+    destDir,
+    buildDir,
+    bundleName,
+    (bundlePath) => !process.env.CI && log(`Antora option: --ui-bundle-url=${bundlePath}`)
+  ),
 })
 
 const bundleTask = createTask({
