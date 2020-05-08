769a848072f532ba2bd15ef171026b9c5ed096da
Dan Allen <dan@opendevise.com>
2020-05-07 18:00:10 -0600

    update template to derive edit URL from properties in Antora 2.3 UI model

--- src/helpers/home-edit-url.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const SEARCH_RX = /^.*(?=\/home\/)/
-
-module.exports = (editUrl, baseUrl, branch) =>
-  editUrl.replace(SEARCH_RX, `${baseUrl}/edit/${branch === 'HEAD' ? 'master' : branch}`)
