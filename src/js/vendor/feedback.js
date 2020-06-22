b777129f976561425b3a68d82395dd1e00b3e6eb
Amarantha Kulkarni <amarantha-k@users.noreply.github.com>
2020-06-22 16:13:01 -0700

    Update issue collector URL

--- src/js/vendor/feedback.js
+++ src/js/vendor/feedback.js
@@ -7,7 +7,7 @@
     var config = document.getElementById('feedback-script').dataset
     var script = document.createElement('script')
     // eslint-disable-next-line max-len
-    script.src = 'https://issues.couchbase.com/s/c388fbe027b748256bfad112765f7a1d-T/-k7v1j/805002/be0e2f3d0946caa44cd62a35c9de2f18/3.1.0/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=' + config.collectorId // prettier-ignore
+    script.src = 'https://issues.couchbase.com/s/4e5aeb929d05d6c94d980322c73e6ee4-T/n8ktcf/805004/be0e2f3d0946caa44cd62a35c9de2f18/4.0.0/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=' + config.collectorId // prettier-ignore
     document.body.appendChild(script)
   })
 })()
